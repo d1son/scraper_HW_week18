37132ab447a9d257619ea3d2cd463db8cf75e3db
d1son <dson120@gmail.com>
2016-03-14 16:33:39 -0400

    cleaned up code to append to tables without removing th

--- public/app.js
+++ public/app.js
@@ -10,16 +10,26 @@ $.getJSON("/all", function(data){
 	$("#weightSort").on("click", function(){
 		//get and log the info from /weight
 		$.getJSON("/weight", function(data){
+			$("#results tbody").empty();
 			for(var i = 0; i < data.length; i++){
-				data[i]
+				var newTr = "<tr>";
+				newTr += "<td>" + data[i].name + "</td>";
+				newTr += "<td>" + data[i].numlegs + "</td>";
+				newTr += "<td>" + data[i].class + "</td>";
+				newTr += "<td>" + data[i].weight + "</td>";
+				newTr += "<td>" + data[i].whatIWouldReallyCallIt + "</td>";
+				newTr += "</tr>";
+				$("#results tbody").append(newTr);
 			}
-			console.log(data);
 		})
 		console.log("weight button clicked")
 	})
 	$("#nameSort").on("click", function(){
 		$.getJSON("/name", function(data){
-			console.log(data)
+			$("#results").empty();
+			for(var i = 0; i < data.length; i++){
+				$("#results tbody").append("<tr><td>" + data[i].name + "</td><td>" + data[i].numlegs + "</td><td>" + data[i].class + "</td><td>" + data[i].weight + "</td><td>" + data[i].whatIWouldReallyCallIt +"</td></tr>")
+			}
 		})
 		console.log("name button clicked")
 	})
