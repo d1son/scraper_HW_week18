0317bf4c5e792a33451bed0a82b633b0df43c471
d1son <dson120@gmail.com>
2016-03-14 12:40:52 -0400

    scrapped the title of reddit using cheerio and request

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var expressHandlebars = require('express-handlebars');
 var bodyParser = require('body-parser');
 var request = require('request');
 var cheerio = require('cheerio');
+var mongojs = require('mongojs');
+var mongoose = require('mongoose');
+var db = mongojs('scraper', ['collection name here']);
 var app = express();
 
 
@@ -27,12 +30,26 @@ app.use(express.static("public"));
 // Request made to reddit.com, displays the info in the body
 
 request('https://www.reddit.com', function (error, response, body) {
+	var results = [];
   if (!error && response.statusCode == 200) {
-	  app.get('/', function(req, res) {
-	    res.send(body); //Sends the scrapped information of reddit to the home route
-	  });
+		$ = cheerio.load(body);
+
+		$('p.title').each(function(i, elem) {
+			results.push({
+		 		title: $(this).text()
+		 	})
+		})
   }
+  console.log(results)
 })
+
+// request('https://www.reddit.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+// 	  app.get('/', function(req, res) {
+// 	    res.send(body); //Sends the scrapped information of reddit to the home route
+// 	  });
+//   }
+// })
 
 
 app.listen(PORT, function(){
