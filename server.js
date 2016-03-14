ab01c0e5aa4b26c66e43714d5ca0a8e2f5871a78
d1son <dson120@gmail.com>
2016-03-13 20:26:36 -0400

    got the scrapped information from reddit to show on home route

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-server.js
+var express = require('express')
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var cheerio = require('cheerio');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+request('https://www.reddit.com', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+	  app.get('/', function(req, res) {
+	    res.send(body); //Sends the scrapped information of reddit to the home route
+	  });
+  }
+})
+
+
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
+});
